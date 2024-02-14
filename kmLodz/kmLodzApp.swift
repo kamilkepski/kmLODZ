7917624acc351dd8dc201e155ac87ca48021c103
Kamil Kępski <kamil.kepskii@gmail.com>
2024-02-14 12:33:24 +0100

    Initial Commit

---
+++ kmLodz/kmLodzApp.swift
@@ -0,0 +1,17 @@
+//
+//  kmLodzApp.swift
+//  kmLodz
+//
+//  Created by Kamil on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct kmLodzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
